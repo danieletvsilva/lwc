8492ce012e8de8494e21ba8b03f90e209beab0b1
Daniele Torres <danieletvsilva@gmail.com>
2023-04-26 17:09:13 -0300

    LWC2_atividade3

---
+++ force-app/main/default/lwc/atividade3child/atividade3child.js
@@ -0,0 +1,8 @@
+import { LightningElement, api } from "lwc";
+
+export default class Lwc_atividade3child extends LightningElement {
+  @api ide;
+  @api lastname;
+  @api company;
+  @api status;
+}
---
+++ force-app/main/default/lwc/lwc_atividade3/lwc_atividade3.js
@@ -0,0 +1,3 @@
+import { LightningElement } from "lwc";
+
+export default class Lwc_atividade3 extends LightningElement {}
