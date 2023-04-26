40ba05afa8bf106a69479aedfd37c07fd7260213
Daniele Torres <danieletvsilva@gmail.com>
2023-04-26 16:26:38 -0300

    LWCII_atividade1

---
+++ force-app/main/default/lwc/lwc_atividade1/lwc_atividade1.js
@@ -0,0 +1,12 @@
+import { LightningElement } from "lwc";
+
+export default class Lwc_atividade1 extends LightningElement {
+  lead = {
+    primeiroNome: "Daniele ",
+    sobrenome: "Torres",
+    idade: 33,
+    email: "danitvsilva@gmail.com",
+    classificacao: "Hot",
+    receitaAnual: 1000000
+  };
+}
